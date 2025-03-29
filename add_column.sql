5ce81fd8be9c2789c98b16a8940b5f13a86b395a
felippe11 <andrefelippe11@hotmail.com>
2025-03-29 16:44:12 -0300

    ajuste

--- add_column.sql
+++
@@ -1,8 +0,0 @@
--- Adicionar a coluna com valor padrão
-ALTER TABLE configuracoes ADD COLUMN pontuacao_minima INTEGER DEFAULT 10;
-
--- Fazer update em todos os registros existentes
-UPDATE configuracoes SET pontuacao_minima = 10 WHERE pontuacao_minima IS NULL;
-
--- Tornar a coluna NOT NULL
-ALTER TABLE configuracoes ALTER COLUMN pontuacao_minima SET NOT NULL; 
